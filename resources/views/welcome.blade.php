a2a7b5e2e7e34a35cbf82b293f50b21396262ff4
Diah Rahman <diahrahman10@gmail.com>
2024-03-11 09:38:26 +0700

    title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>D Pojok Garden</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
