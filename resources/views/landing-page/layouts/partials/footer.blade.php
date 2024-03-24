a55a9767368dece3cdc5a7d883b3d524686c6a01
Diah Rahman <diahrahman10@gmail.com>
2024-03-24 08:24:46 +0700

    landing-rev

--- resources/views/landing-page/layouts/partials/footer.blade.php
+++ resources/views/landing-page/layouts/partials/footer.blade.php
@@ -4,7 +4,7 @@
         <div class="row g-5">
             <div class="col-lg-3 col-md-6">
                 <h4 class="text-white mb-4">Our Office</h4>
-                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
+                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Gg Sukagalih No.1 No 5, Karanganyar, Subang</p>
                 <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
                 <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
                 <div class="d-flex pt-2">
---
+++ resources/views/user/admin/unit/unit.blade.php
@@ -0,0 +1,237 @@
+@extends('layouts.main.layout')
+@section('title')
+    Kelola Satuan Bahan Baku
+@endsection
+@section('styles')
+ <!-- Notification.css -->
+ <link rel="stylesheet" type="text/css" href="{{ asset('main') }}/assets/pages/notification/notification.css">
+@endsection
+@section('content')
+<div class="card">
+    <div class="card-header">
+        <h5>Satuan Bahan Baku</h5>
+        {{-- <span>use class <code>table-hover</code> inside table element</span> --}}
+        <div class="card-header-right">
+            <ul class="list-unstyled card-option">
+                <li><i class="fa fa-chevron-left"></i></li>
+                <li><i class="fa fa-window-maximize full-card"></i></li>
+                <li><i class="fa fa-minus minimize-card"></i></li>
+                <li><i class="fa fa-refresh reload-card"></i></li>
+                <li><i class="fa fa-times close-card"></i></li>
+            </ul>
+        </div>
+
+    </div>
+    <div class="card-block table-border-style">
+        @if($errors->any())
+            {!! implode('', $errors->all('<div style="color: red;">:message</div>')) !!}
+        @endif
+        <br>
+        <button type="button" class="btn btn-sm btn-info btn-round" data-toggle="modal" data-target="#tambahModal">
+            Tambah Satuan Bahan Baku
+        </button>
+        <br>
+        <div class="table-responsive">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Satuan</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($unit as $u)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $u->satuan }}</td>
+                            <td>
+                                <button type="button" class="btn btn-primary btn-round btn-sm" data-toggle="modal" data-target="#editModal{{ $u->id }}">
+                                    Edit
+                                  </button>
+                                <button type="button" class="btn btn-danger btn-round btn-sm" data-toggle="modal" data-target="#hapusModal{{ $u->id }}">
+                                    Hapus
+                                </button>
+                            </td>
+                        </tr>
+                        <!-- Modal Edit -->
+                        <div class="modal fade" id="editModal{{ $u->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Edit Satuan Bahan Baku</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                </div>
+                                <div class="modal-body">
+                                    <form action="{{ route('unit.update', $u->id) }}" method="POST">
+                                        @method('PUT')
+                                        @csrf
+                                        <div class="form-group">
+                                        <label for="recipient-name" class="col-form-label">Nama Satuan</label>
+                                        <input type="text" class="form-control" placeholder="Nama Satuan" name="satuan" required value="{{ $u->satuan }}">
+                                        </div>
+                                </div>
+                                <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                                <button type="submit" class="btn btn-primary">Simpan</button>
+                                </form>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        <!-- Modal Hapus -->
+                        <div class="modal fade" id="hapusModal{{ $u->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Konfirmasi Penghapusan</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                </div>
+                                <div class="modal-body">
+                                    Apakah Anda yakin untuk menghapus satuan {{ $u->satuan }} ?
+                                </div>
+                                <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                                <form action="{{ route('unit.destroy', $u->id) }}" method="POST">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit" class="btn btn-danger">Hapus</button>
+                                </form>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    @empty
+                        <tr>
+                            <td colspan="4" class="text-center">No Data</td>
+                        </tr>
+                    @endforelse
+
+                </tbody>
+            </table>
+        </div>
+        <!-- Modal Tambah -->
+        <div class="modal fade" id="tambahModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Satuan Bahan Baku</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('unit.store') }}" method="POST">
+                        @csrf
+                        <div class="form-group">
+                          <label for="recipient-name" class="col-form-label">Nama Satuan</label>
+                          <input type="text" class="form-control" placeholder="Nama Satuan" name="satuan" required>
+                        </div>
+                </div>
+                <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                <button type="submit" class="btn btn-primary">Simpan</button>
+                </form>
+                </div>
+            </div>
+            </div>
+        </div>
+            </div>
+            <div id="styleSelector">
+
+            </div>
+        </div>
+@endsection
+@section('scripts')
+<!-- notification js -->
+<script type="text/javascript" src="{{ asset('main/assets/js/bootstrap-growl.min.js') }}"></script>
+<script>
+    function notify(from, align, icon, type, animIn, animOut, msg, title){
+        $.growl({
+            icon: icon,
+            title: title,
+            message: msg,
+            url: ''
+        },{
+            element: 'body',
+            type: type,
+            allow_dismiss: true,
+            placement: {
+                from: from,
+                align: align
+            },
+            offset: {
+                x: 30,
+                y: 30
+            },
+            spacing: 10,
+            z_index: 999999,
+            delay: 2500,
+            timer: 1000,
+            url_target: '_blank',
+            mouse_over: false,
+            animate: {
+                enter: animIn,
+                exit: animOut
+            },
+            icon_type: 'class',
+            template: '<div data-growl="container" class="alert" role="alert">' +
+            '<button type="button" class="close" data-growl="dismiss">' +
+            '<span aria-hidden="true">&times;</span>' +
+            '<span class="sr-only">Close</span>' +
+            '</button>' +
+            '<span data-growl="icon"></span>' +
+            '<span data-growl="title"></span>' +
+            '<span data-growl="message"></span>' +
+            '<a href="#" data-growl="url"></a>' +
+            '</div>'
+        });
+    };
+
+
+    // $('#notif').on('click',function(e){
+    //     e.preventDefault();
+    //     var nFrom = $(this).attr('data-from');
+    //     var nAlign = $(this).attr('data-align');
+    //     var nIcons = $(this).attr('data-icon');
+    //     var nType = $(this).attr('data-type');
+    //     var nAnimIn = $(this).attr('data-animation-in');
+    //     var nAnimOut = $(this).attr('data-animation-out');
+
+    //     notify(nFrom, nAlign, nIcons, nType, nAnimIn, nAnimOut);
+    // });
+</script>
+@if (Session::has('success'))
+<script>
+var nFrom = 'top';
+var nAlign = 'right';
+var nIcons = 'fa fa-check';
+var nType = 'success';
+var nAnimIn = 'animated fadeIn';
+var nAnimOut = 'animated fadeInLeft';
+var msg = "{{ Session::get('success') }}";
+var title = "Sukses! ";
+
+notify(nFrom, nAlign, nIcons, nType, nAnimIn, nAnimOut, msg, title);
+</script>
+@endif
+
+@if (Session::has('fail'))
+<script>
+var nFrom = 'top';
+var nAlign = 'right';
+var nIcons = 'fa fa-comments';
+var nType = 'danger';
+var nAnimIn = 'animated fadeIn';
+var nAnimOut = 'animated fadeInLeft';
+var msg = "{{ Session::get('fail') }}";
+var title = "Gagal! ";;
+
+notify(nFrom, nAlign, nIcons, nType, nAnimIn, nAnimOut, msg, title);
+</script>
+@endif
+@endsection
