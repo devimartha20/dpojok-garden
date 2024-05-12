ffdb842d3835be07d5f1b60e4cd7b4071d38a8e8
Diah Rahman <diahrahman10@gmail.com>
2024-05-12 19:45:27 +0700

    cuyy

--- resources/views/user/admin/attendance/absensi.blade.php
+++ resources/views/user/admin/attendance/absensi.blade.php
@@ -1,40 +1,43 @@
 @extends('layouts.main.layout')
 @section('title')
-    Absensi Pegawai
+    Daftar Form Absensi
 @endsection
 @section('styles')
  <!-- Notification.css -->
  <link rel="stylesheet" type="text/css" href="{{ asset('main') }}/assets/pages/notification/notification.css">
 @endsection
 @section('content')
+<div class="row">
+    <h5>Daftar Pengajuan Form Absensi</h5>
+    <br>
+</div>
 <div class="page-header card">
     <div class="card-block">
-        <h5 class="m-b-10">Daftar Pengajuan Absensi Pegawai</h5>
-        <p class="text-muted m-b-10">Daftar pengajuan absensi pegawai</p>
-        <ul class="breadcrumb-title b-t-default p-t-10">
-            <li class="breadcrumb-item">
-                <a href="index.html"> <i class="fa fa-home"></i> </a>
-            </li>
-            <li class="breadcrumb-item"><a href="#!">Basic Components</a>
-            </li>
-            <li class="breadcrumb-item"><a href="#!">Breadcrumb</a>
-            </li>
-            <li class="breadcrumb-item">
-                <a href="#!">
-                    <i class="ti-check"></i>
-                </a>
-            </li>
-            <li class="breadcrumb-item">
-                <a href="#!">
-                    <i class="ti-close"></i>
-                </a>
-            </li>
-            <li class="breadcrumb-item">
-                <a href="#!">
-                    <i class="ti-save"></i>
-                </a>
-            </li>
-        </ul>
+        <h6 class="m-b-10">0021 - Debi</h6><hr>
+        <p>Tipe : Absen Masuk</p>
+        <p>Catatan : absen masuk pagi ini</p>
+        <div class="float-right">
+            <select class="form-control" id="statusDebi">
+                <option value="approve">Disetujui</option>
+                <option value="reject">Ditolak</option>
+                <option value="pending">Pending</option>
+            </select>
+        </div>
+    </div>
+</div>
+
+<div class="page-header card">
+    <div class="card-block">
+        <h6 class="m-b-10">0023 - Deni</h6><hr>
+        <p>Tipe : Absen Pulang</p>
+        <p>Catatan : absen pulang malam ini</p>
+        <div class="float-right">
+            <select class="form-control" id="statusDeni">
+                <option value="approve">Disetujui</option>
+                <option value="reject">Ditolak</option>
+                <option value="pending">Pending</option>
+            </select>
+        </div>
     </div>
 </div>
 
