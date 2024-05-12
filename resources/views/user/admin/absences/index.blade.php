6fb6cc7a97420c250b03394433d0ecb9123762b0
devimartha20 <devimarthareal@gmail.com>
2024-05-12 23:38:13 +0700

    ..

--- resources/views/user/admin/absences/index.blade.php
+++ resources/views/user/admin/absences/index.blade.php
@@ -15,108 +15,184 @@
             <div class="slide"></div>
         </li>
         <li class="nav-item">
-            <a class="nav-link" data-toggle="tab" href="#profile3" role="tab">Disetujui</a>
+            <a class="nav-link" data-toggle="tab" href="#confirmed" role="tab">Disetujui</a>
             <div class="slide"></div>
         </li>
         <li class="nav-item">
-            <a class="nav-link" data-toggle="tab" href="#settings3" role="tab">Ditolak</a>
+            <a class="nav-link" data-toggle="tab" href="#rejected" role="tab">Ditolak</a>
             <div class="slide"></div>
         </li>
     </ul>
     <!-- Tab panes -->
     <div class="tab-content card-block">
-        <div class="tab-pane active" id="home3" role="tabpanel">
-            <div class="page-header card">
-                <div class="card-block">
-                    <h6 class="m-b-10">0021 - Debi</h6><hr>
-                    <p>Tanggal Awal: 09-08-2024</p>
-                    <p>Tanggal Akhir : 10-08-2024</p>
-                    <p>Keterangan : Sakit</p>
-                    <div class="float-right">
-                        <button class="btn btn-primary change-status" data-toggle="modal" data-target="#data1">Ubah Status</button>
+        <div class="tab-pane active" id="pending" role="tabpanel">
+           
+                @forelse ($pending_absences as $pa)
+                <div class="page-header card">
+                    <div class="card-block">
+                        <h6 class="m-b-10">{{ $pa->employee->id_pegawai }} - {{ $pa->employee->nama }}</h6><hr>
+                        <p>Tanggal Awal: {{ $pa->start_date }}</p>
+                        <p>Tanggal Akhir : {{ $pa->end_date }}</p>
+                        <p>Alasan : {{ $pa->reason }}</p>
+                        <p>Keterangan : {{ $pa->keterangan ?? '-' }}</p>
+                        <div class="float-right">
+                            <button class="btn btn-primary change-status" data-toggle="modal" data-target="#data{{ $pa->id }}">Ubah Status</button>
+                        </div>
                     </div>
-                </div>
-            </div>
+                    <div class="modal fade" id="data{{ $pa->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-xl" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Ubah Satatus Ketidakhadiran</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            </div>
+                            <div class="modal-body">
+                                <form method="POST" action="{{ route('absence.update.status', $pa->id) }}">
+                                    @csrf
+                                    <div class="mb-3">
+                                        <label for="status" class="form-label"  >Status</label>
+                                        <select class="form-select" id="status" name="status">
+                                            <option value="confirmed" {{ $pa->status == 'confirmed' ? 'selected' : '' }}>Diterima</option>
+                                            <option value="rejected" {{ $pa->status == 'rejected' ? 'selected' : '' }}>Ditolak</option>
+                                            <option value="pending" {{ $pa->status == 'pending' ? 'selected' : '' }}>Menunggu</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="catatan" class="form-label">Catatan</label>
+                                        <textarea  name="catatan" class="form-control" id="catatan" rows="3"></textarea>
+                                    </div>
+                                
+                            </div>
+                            <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary">Simpan</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                            </div>
+                        </form>
+                        </div>
+                        </div>
+                    </div>
+                 </div>
+                @empty
+                    
+                @endforelse
+                
+            
         </div>
-        <div class="tab-pane" id="profile3" role="tabpanel">
-            <div class="page-header card">
-                <div class="card-block">
-                    <h6 class="m-b-10">0021 - Debi</h6><hr>
-                    <p>Tanggal Awal: 09-08-2024</p>
-                    <p>Tanggal Akhir : 10-08-2024</p>
-                    <p>Keterangan : Sakit</p>
-                    <div class="float-right">
-                        <button class="btn btn-primary change-status" data-toggle="modal" data-target="#data1">Ubah Status</button>
+        <div class="tab-pane" id="confirmed" role="tabpanel">
+            @forelse ($confirmed_absences as $ca)
+                <div class="page-header card">
+                    <div class="card-block">
+                        <h6 class="m-b-10">{{ $ca->employee->id_pegawai }} - {{ $ca->employee->nama }}</h6><hr>
+                        <p>Tanggal Awal: {{ $ca->start_date }}</p>
+                        <p>Tanggal Akhir : {{ $ca->end_date }}</p>
+                        <p>Alasan : {{ $ca->reason }}</p>
+                        <p>Keterangan : {{ $ca->keterangan ?? '-' }}</p>
+                        <div class="float-right">
+                            <button class="btn btn-primary change-status" data-toggle="modal" data-target="#data{{ $ca->id }}">Ubah Status</button>
+                        </div>
                     </div>
-                </div>
-            </div>
+                    <div class="modal fade" id="data{{ $ca->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-xl" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Ubah Satatus Ketidakhadiran</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            </div>
+                            <div class="modal-body">
+                                <form method="POST" action="{{ route('absence.update.status', $ca->id) }}">
+                                    @csrf
+                                    <div class="mb-3">
+                                        <label for="status" class="form-label"  >Status</label>
+                                        <select class="form-select" id="status" name="status">
+                                            <select class="form-select" id="status" name="status">
+                                                <option value="confirmed" {{ $ca->status == 'confirmed' ? 'selected' : '' }}>Diterima</option>
+                                                <option value="rejected" {{ $ca->status == 'rejected' ? 'selected' : '' }}>Ditolak</option>
+                                                <option value="pending" {{ $ca->status == 'pending' ? 'selected' : '' }}>Menunggu</option>
+                                            </select>
+                                        </select>
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="catatan" class="form-label">Catatan</label>
+                                        <textarea class="form-control" id="catatan" name="catatan" rows="3"></textarea>
+                                    </div>
+                                
+                            </div>
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-primary">Simpan</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                            </div>
+                        </form>
+                        </div>
+                        </div>
+                    </div>
+                 </div>
+                @empty
+                    
+                @endforelse
         </div>
         <div class="tab-pane" id="settings3" role="tabpanel">
+            @forelse ($rejeted_absences as $ra)
             <div class="page-header card">
                 <div class="card-block">
-                    <h6 class="m-b-10">0021 - Debi</h6><hr>
-                    <p>Tanggal Awal: 09-08-2024</p>
-                    <p>Tanggal Akhir : 10-08-2024</p>
-                    <p>Keterangan : Sakit</p>
+                    <h6 class="m-b-10">{{ $ra->employee->id_pegawai }} - {{ $ra->employee->nama }}</h6><hr>
+                    <p>Tanggal Awal: {{ $ra->start_date }}</p>
+                    <p>Tanggal Akhir : {{ $ra->end_date }}</p>
+                    <p>Alasan : {{ $ra->reason }}</p>
+                    <p>Keterangan : {{ $ra->keterangan ?? '-' }}</p>
                     <div class="float-right">
-                        <button class="btn btn-primary change-status" data-toggle="modal" data-target="#data1">Ubah Status</button>
+                        <button class="btn btn-primary change-status" data-toggle="modal" data-target="#data{{ $ca->id }}">Ubah Status</button>
                     </div>
                 </div>
-            </div>
+                <div class="modal fade" id="data{{ $ca->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-xl" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Ubah Satatus Ketidakhadiran</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        </div>
+                        <div class="modal-body">
+                            <form method="POST" action="{{ route('absence.update.status', $ra->id) }}">
+                                @csrf
+                                <div class="mb-3">
+                                    <label for="status" class="form-label">Status</label>
+                                    <select class="form-select" id="status" name="status">
+                                        <option value="confirmed" {{ $ra->status == 'confirmed' ? 'selected' : '' }}>Diterima</option>
+                                        <option value="rejected" {{ $ra->status == 'rejected' ? 'selected' : '' }}>Ditolak</option>
+                                        <option value="pending" {{ $ra->status == 'pending' ? 'selected' : '' }}>Menunggu</option>
+                                    </select>
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="catatan" class="form-label">Catatan</label>
+                                    <textarea class="form-control" id="catatan" name="catatan" rows="3"></textarea>
+                                </div>
+                           
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary">Simpan</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                        </div>
+                    </form>
+                    </div>
+                    </div>
+                </div>
+             </div>
+            @empty
+                
+            @endforelse
         </div>
     </div>
 </div>
 {{-- Modal  --}}
-<div class="modal fade" id="data1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-xl" role="document">
-    <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Ubah Satatus Ketidakhadiran</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-        </div>
-        <div class="modal-body">
-            <form>
-                <div class="mb-3">
-                    <label for="kode_pegawai" class="form-label">Kode Pegawai</label>
-                    <input type="text" class="form-control" id="kode_pegawai" readonly>
-                </div>
-                <div class="mb-3">
-                    <label for="nama_pegawai" class="form-label">Nama Pegawai</label>
-                    <input type="text" class="form-control" id="nama_pegawai" readonly>
-                </div>
-                <div class="mb-3">
-                    <label for="tanggal_awal" class="form-label">Tanggal Awal</label>
-                    <input type="text" class="form-control" id="tanggal_awal" readonly>
-                </div>
-                <div class="mb-3">
-                    <label for="tanggal_akhir" class="form-label">Tanggal Akhir</label>
-                    <input type="text" class="form-control" id="tanggal_akhir" readonly>
-                </div>
-                <div class="mb-3">
-                    <label for="keterangan" class="form-label">Keterangan</label>
-                    <input type="text" class="form-control" id="keterangan" readonly>
-                </div>
-                <div class="mb-3">
-                    <label for="status" class="form-label">Status</label>
-                    <select class="form-select" id="status">
-                        <option value="Diterima">Diterima</option>
-                        <option value="Ditolak">Ditolak</option>
-                    </select>
-                </div>
-                <div class="mb-3">
-                    <label for="catatan" class="form-label">Alasan</label>
-                    <textarea class="form-control" id="catatan" rows="3"></textarea>
-                </div>
-            </form>
-        </div>
-        <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-        </div>
-    </div>
-    </div>
-</div>
+
 @endsection
 @section('scripts')
 <!-- notification js -->
