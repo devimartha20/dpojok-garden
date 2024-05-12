07fcb00d2798be875a55cb0eada164333d0c5615
Diah Rahman <diahrahman10@gmail.com>
2024-05-12 23:57:45 +0700

    cc

--- resources/views/user/admin/attendance/index.blade.php
+++ resources/views/user/admin/attendance/index.blade.php
@@ -289,8 +289,8 @@
                                 </div>
                         </div>
                         <div class="modal-footer">
-                            <button type="submit" class="btn btn-secondary mr-auto" data-dismiss="modal">Ubah</button>
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                            <button type="submit" class="btn btn-primary">Simpan</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
                         </div>
                     </form>
                     </div>
