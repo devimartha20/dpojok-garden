7494fa752f106168a1f52ad4f093f80e764f1f72
Diah Rahman <diahrahman10@gmail.com>
2024-05-19 18:47:41 +0700

    xi

---
+++ resources/views/user/kasir/reservasi/pay-reserv.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts.main.layout')
+@section('title')
+Pembayaran Reservasi
+@endsection
+div class="container">
+        <div class="card shadow-lg">
+            <div class="card-header bg-primary text-white">
+                <h5 class="card-title">Pembayaran Nomor 1716118775-ORDER-02024051918392822</h5>
+            </div>
+            <div class="card-body">
+                <!--[if BLOCK]><![endif]-->                    <div class="alert alert-success">
+                        Pemesanan berhasil disimpan, silahkan lakukan pembayaran!
+                    </div>
+                <!--[if ENDBLOCK]><![endif]-->
+                <!--[if BLOCK]><![endif]--><!--[if ENDBLOCK]><![endif]-->
+
+                <!--[if BLOCK]><![endif]-->                    <div class="text-right mb-4">
+                        <a class="btn btn-info" href="https://dpodjok-garden.marthashares-project.my.id/print/invoice/5" target="_blank">Print Invoice</a>
+                    </div>
+                    <form wire:submit.prevent="pay">
+                        <div class="form-group">
+                            <label for="payment_method">Metode Pembayaran</label>
+                            <select class="form-control" id="payment_method" wire:model.live="payment_method" required="">
+                                <option value="cash" selected="">Tunai</option>
+                                <option value="qris">QRIS</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="transaction_number">Nomor Transaksi</label>
+                            <input type="text" class="form-control" id="transaction_number" wire:model.live="transaction_number" readonly="" required="">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="uang">Total Uang Diberikan</label>
+                            <input type="number" min="10000" class="form-control" id="uang" wire:model.lazy="uang" required="">
+                        </div>
+
+                        <!-- Display total_bayar -->
+                        <div class="row mb-3">
+                            <div class="col-6">
+                                <div class="d-flex align-items-center">
+
+                                    <span class="font-weight-bold">Total Harga:</span>
+                                </div>
+                            </div>
+                            <div class="col-6 text-right">
+                                <div class="font-weight-bold">Rp. 10,000</div>
+                            </div>
+                        </div>
+                        <!-- Display Kembali -->
+                        <div class="row mb-3">
+                            <div class="col-6">
+                                <div class="d-flex align-items-center">
+                                    <i class="fas fa-money-bill-wave mr-2 text-success"></i>
+                                    <span class="font-weight-bold">Kembali:</span>
+                                </div>
+                            </div>
+                            <div class="col-6 text-right">
+                                <div class="font-weight-bold">Rp. 0</div>
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Bayar</button>
+                    </form>
+                <!--[if ENDBLOCK]><![endif]-->
+                <div class="text-right">
+                    <a href="https://dpodjok-garden.marthashares-project.my.id/ordertrans" class="btn btn-secondary">Kembali</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</div>
+<div class="card-body">
+    <!--[if BLOCK]><![endif]-->                    <div class="alert alert-success">
+            Pembayaran berhasil, Pesanan sudah masuk ke daftar antrian!
+        </div>
+    <!--[if ENDBLOCK]><![endif]-->
+    <!--[if BLOCK]><![endif]--><!--[if ENDBLOCK]><![endif]-->
+
+    <!--[if BLOCK]><![endif]-->                    <div class="text-right mb-4">
+            <a class="btn btn-success mr-2" href="https://dpodjok-garden.marthashares-project.my.id/print/receipt/5" target="_blank">Print Receipt</a>
+            <a class="btn btn-info" href="https://dpodjok-garden.marthashares-project.my.id/print/invoice/5" target="_blank">Print Invoice</a>
+        </div>
+        <div>
+            <p class="mb-2">Status Pembayaran: <span class="font-weight-bold">lunas</span></p>
+            <p class="mb-2">Uang: Rp. 20,000</p>
+            <p class="mb-2">Kembali: Rp. 10,000</p>
+        </div>
+    <!--[if ENDBLOCK]><![endif]-->
+    <div class="text-right">
+        <a href="https://dpodjok-garden.marthashares-project.my.id/ordertrans" class="btn btn-secondary">Kembali</a>
+    </div>
+</div>
---
+++ resources/views/user/owner/report/attendance.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.main.layout')
+
+@section('styles')
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,700" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('report') }}/css/style.css">
+@endsection
+
+@section('content')
+<section class="">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <h3 class="h5 mb-4 text-center">Laporan Penjualan</h3>
+                <form method="GET" action="#">
+                    <div class="form-row align-items-center">
+                        <div class="col-auto">
+                            <label for="start_date">Start Date:</label>
+                            <input type="date" name="start_date" class="form-control mb-2" id="start_date" required>
+                        </div>
+                        <div class="col-auto">
+                            <label for="end_date">End Date:</label>
+                            <input type="date" name="end_date" class="form-control mb-2" id="end_date" required>
+                        </div>
+                        <div class="col-auto">
+                            <button type="submit" class="btn btn-primary mb-2">Filter</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="table-wrap">
+                    <table class="table">
+                        <thead class="thead-primary">
+                            <tr>
+                                <th>No</th>
+                                <th>Id Pegawai</th>
+                                <th>Nama Pegawai</th>
+                                <th>Sakit (Hari)</th>
+                                <th>Izin (Hari)</th>
+                                <th>Tanpa Keterangan (Hari)</th>
+                                <th>Cuti (Kali)</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- Row 1 -->
+                            <tr class="alert" role="alert">
+                                <td>1</td>
+                                <td>
+                                    <div class="img" style="background-image: url('images/product-1.png');"></div>
+                                </td>
+                                <td>Spagettie Bolognesse</td>
+                                <td>$44.99</td>
+                                <td>3</td>
+                                <td>$134.97</td>
+                            </tr>
+                            <!-- Row 2 -->
+                            <tr class="alert" role="alert">
+                                <td>2</td>
+                                <td>
+                                    <div class="img" style="background-image: url('images/product-1.png');"></div>
+                                </td>
+                                <td>Cireng</td>
+                                <td>$29.99</td>
+                                <td>2</td>
+                                <td>$59.98</td>
+                            </tr>
+                            <!-- Row 3 -->
+                            <tr class="alert" role="alert">
+                                <td>3</td>
+                                <td>
+                                    <div class="img" style="background-image: url('images/product-1.png');"></div>
+                                </td>
+                                <td>Spagettie Bolognesse</td>
+                                <td>$44.99</td>
+                                <td>1</td>
+                                <td>$44.99</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
