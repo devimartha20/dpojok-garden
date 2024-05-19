ef9b8e0a1e54ecbed8ae39de6295a64059d40084
devimartha20 <devimarthareal@gmail.com>
2024-05-19 18:59:24 +0700

    ..

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -15,24 +15,28 @@ class ReportController extends Controller
 {
     public function salesReport(Request $request)
     {
-        // Retrieve the start and end date from the request
-        $startDate = $request->input('start_date');
-        $endDate = $request->input('end_date');
-
-        // Query to fetch sales data grouped by product
-        $salesData = DetailOrder::select(
-                'products.nama as product_name',
-                'products.harga_jual as product_price',
-                DB::raw('SUM(detail_orders.jumlah) as total_quantity'),
-                DB::raw('SUM(detail_orders.total_harga) as total_sales')
-            )
-            ->join('products', 'detail_orders.product_id', '=', 'products.id')
-            ->whereBetween('detail_orders.created_at', [$startDate, $endDate])
-            ->groupBy('products.nama', 'products.harga_jual')
-            ->get();
-
-        // Pass the sales data and the filter dates to the view
-        return view('user.owner.report.sales', compact('salesData', 'startDate', 'endDate'));
+         // Retrieve the start and end date from the request
+         $startDate = $request->input('start_date');
+         $endDate = $request->input('end_date');
+ 
+         // Set default date range to current month if not provided
+         if (!$startDate) {
+             $startDate = Carbon::now()->startOfMonth()->toDateString();
+         }
+         if (!$endDate) {
+             $endDate = Carbon::now()->endOfMonth()->toDateString();
+         }
+ 
+         // Fetch sales data grouped by product within the date range
+         $salesData = DetailOrder::selectRaw('products.id, products.image, products.nama, products.harga_jual, SUM(detail_orders.jumlah) as total_quantity, SUM(detail_orders.total_harga) as total_sales')
+             ->join('products', 'detail_orders.product_id', '=', 'products.id')
+             ->join('orders', 'detail_orders.order_id', '=', 'orders.id')
+             ->whereBetween('orders.created_at', [$startDate, $endDate])
+             ->groupBy('products.id', 'products.image', 'products.nama', 'products.harga_jual')
+             ->get();
+ 
+         // Pass the sales data and the filter dates to the view
+         return view('user.owner.report.sales', compact('salesData', 'startDate', 'endDate'));
     }
 
     public function attendanceReport(Request $request){
