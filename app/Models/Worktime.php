67e4753b61391674fe62ca8cc595fc985f4537fa
Martha Shares <110329956+devimartha20@users.noreply.github.com>
2024-05-24 16:00:21 +0700

    ..

--- app/Models/Worktime.php
+++ app/Models/Worktime.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Worktime extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'day',
+        'start_time',
+        'end_time',
+        'rest_start_time',
+        'rest_end_time',
+        'rest_duration_min',
+        'working_duration_min',
+        'total_duration_min',
+    ];
+
 }
