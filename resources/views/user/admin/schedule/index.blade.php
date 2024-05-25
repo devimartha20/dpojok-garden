31cc279e5d04334ed4885590b2c95d7a9bf63f3f
devimartha20 <devimarthareal@gmail.com>
2024-05-25 20:54:20 +0700

    ..

--- resources/views/user/admin/schedule/index.blade.php
+++ resources/views/user/admin/schedule/index.blade.php
@@ -6,11 +6,6 @@
     <link href="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.0/main.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.0/main.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.0/locales-all.min.js"></script>
-@endsection
-@section('content')
-    <div id="calendar"></div>
-@endsection
-@section('scripts')
     <script>
         // Convert PHP array to JSON
         var events = @json($events);
@@ -18,10 +13,16 @@
         document.addEventListener('DOMContentLoaded', function() {
             var calendarEl = document.getElementById('calendar');
             var calendar = new FullCalendar.Calendar(calendarEl, {
-                initialView: 'timeGridWeek',
+                initialView: 'dayGridMonth',
                 events: events // Pass events as JSON
             });
             calendar.render();
         });
     </script>
+@endsection
+@section('content')
+    <div id="calendar"></div>
+@endsection
+@section('scripts')
+
 @endsection
