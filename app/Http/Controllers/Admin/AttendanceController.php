05d753c2ac0c3aab52c3bd3705a0bd7d992e318e
devimartha20 <devimarthareal@gmail.com>
2024-06-02 22:10:48 +0700

    ..

--- app/Http/Controllers/Admin/AttendanceController.php
+++ app/Http/Controllers/Admin/AttendanceController.php
@@ -102,10 +102,10 @@ class AttendanceController extends Controller
 
     public function index(Request $request) {
         $request->validate([
-            'start_date' => 'required|date',
-            'end_date' => 'required|date|after_or_equal:start_date',
+            'start_date' => 'date',
+            'end_date' => 'date|after_or_equal:start_date',
         ]);
-        
+
         $start_date = Carbon::parse($request->input('start_date'));
         $end_date = Carbon::parse($request->input('end_date'));
         $reportData = $this->generateEmployeeStatusReport($start_date, $end_date);
