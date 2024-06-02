bed642fb7fec4654adb861bf3dfacb85c5e873ed
Diah Rahman <diahrahman10@gmail.com>
2024-06-02 15:09:07 +0700

    c

--- resources/views/user/admin/employee/create.blade.php
+++ resources/views/user/admin/employee/create.blade.php
@@ -46,7 +46,7 @@
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label">id_pegawai</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" placeholder="id_pegawai" required value="{{ old('id_pegawai') }}" name="id_pegawai" required>
+                        <input type="text" class="form-control" placeholder="ID Pegawai" required value="{{ old('id_pegawai') }}" name="id_pegawai" required>
                     </div>
                 </div>
                 <div class="form-group row">
