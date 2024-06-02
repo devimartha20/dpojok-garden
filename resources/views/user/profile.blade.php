0cc16f1bc8ab89e046d3df3550aa1cb127943ea9
devimartha20 <devimarthareal@gmail.com>
2024-06-03 00:41:28 +0700

    ..

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -56,11 +56,11 @@
                 @include('profile.partials.update-password-form')
             </div>
         </div>
-        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg mt-4">
+        {{-- <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg mt-4">
             <div class="max-w-xl">
                 @include('profile.partials.delete-user-form')
             </div>
-        </div>
+        </div> --}}
     </div>
 </div>
 @endsection
