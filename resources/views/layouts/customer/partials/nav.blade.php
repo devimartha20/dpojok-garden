05b7c32334034cc4110a786224ca163b90f352af
devimartha <devimarthareal@gmail.com>
2024-06-09 18:23:26 +0700

    ..

--- resources/views/layouts/customer/partials/nav.blade.php
+++ resources/views/layouts/customer/partials/nav.blade.php
@@ -47,17 +47,17 @@
 </div>
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
     <div class="container">
-      <a class="navbar-brand" href="index.html">D'Podjok Garden</a>
+      <a class="navbar-brand" href="/">D'Podjok Garden</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="oi oi-menu"></span> Menu
       </button>
 
       <div class="collapse navbar-collapse" id="ftco-nav">
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
-            <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
-            <li class="nav-item"><a href="menu.html" class="nav-link">Menu</a></li>
-          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+            <li class="nav-item active"><a href="/" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="#about" class="nav-link">About</a></li>
+            <li class="nav-item"><a href="#menu" class="nav-link">Menu</a></li>
+          <li class="nav-item"><a href="#contact" class="nav-link">Contact</a></li>
           <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Sign Up</a></li>
           <li class="nav-item cta"><a href="{{ route('login') }}" class="nav-link">Login</a></li>
         </ul>
