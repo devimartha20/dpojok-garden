7fcc9600b81f97ed8828555a156e9f3b1e8ce2a4
devimartha <devimarthareal@gmail.com>
2024-06-09 20:29:50 +0700

    ..

--- resources/views/layouts/customer/partials/nav.blade.php
+++ resources/views/layouts/customer/partials/nav.blade.php
@@ -31,11 +31,11 @@
                 <div class="row d-flex">
                     <div class="col-md pr-4 d-flex topper align-items-center">
                         <div class="icon mr-2 d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
-                        <span class="text">081322455758</span>
+                        <span class="text">+62 813-2245-5758</span>
                     </div>
                     <div class="col-md pr-4 d-flex topper align-items-center">
-                        <div class="icon mr-2 d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
-                        <span class="text">youremail@email.com</span>
+                        <div class="icon mr-2 d-flex justify-content-center align-items-center"><span class="icon-home"></span></div>
+                        <span class="text">Karanganyar, Subang Regency</span>
                     </div>
                     <div class="col-md-5 pr-4 d-flex topper align-items-center text-lg-right justify-content-end">
                         <p class="mb-0 register-link"><span>Open hours:</span> <span>Monday - Sunday</span> <span>10:00AM - 10:00PM</span></p>
