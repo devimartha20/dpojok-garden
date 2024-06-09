1ec346209961e20b88e3466732bb89ebb6f3b923
devimartha <devimarthareal@gmail.com>
2024-06-09 20:43:10 +0700

    ..

--- resources/views/layouts/customer/partials/nav.blade.php
+++ resources/views/layouts/customer/partials/nav.blade.php
@@ -54,12 +54,12 @@
 
       <div class="collapse navbar-collapse" id="ftco-nav">
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item active"><a href="/" class="nav-link">Home</a></li>
+            <li class="nav-item active"><a href="#" class="nav-link">Home</a></li>
             <li class="nav-item"><a href="#about" class="nav-link">About</a></li>
             <li class="nav-item"><a href="#menu" class="nav-link">Menu</a></li>
-          <li class="nav-item"><a href="#contact" class="nav-link">Contact</a></li>
-          <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Sign Up</a></li>
-          <li class="nav-item cta"><a href="{{ route('login') }}" class="nav-link">Login</a></li>
+            <li class="nav-item"><a href="#contact" class="nav-link">Contact</a></li>
+            <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Sign Up</a></li>
+            <li class="nav-item cta"><a href="{{ route('login') }}" class="nav-link">Login</a></li>
         </ul>
       </div>
     </div>
