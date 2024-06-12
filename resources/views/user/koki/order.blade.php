6d0b50a321f01f6fa2d0d2b4aed0faf256c4a7ed
Martha Shares <110329956+devimartha20@users.noreply.github.com>
2024-06-12 15:42:46 +0700

    ..

--- resources/views/user/koki/order.blade.php
+++ resources/views/user/koki/order.blade.php
@@ -4,8 +4,15 @@
 @endsection
 @section('styles')
 <style>
-    .nav-tabs.horizontal-tabs li {
-    display: inline-block;
+ .nav-tabs.horizontal-tabs {
+    display: flex;
+    justify-content: center;
+    flex-wrap: nowrap; /* Prevent wrapping of items */
+    overflow-x: auto; /* Allow horizontal scrolling if needed */
+}
+
+.nav-tabs.horizontal-tabs li {
+    flex: 0 0 auto; /* Prevent items from growing or shrinking */
     margin-right: 10px; /* Adjust spacing between items as needed */
 }
 </style>
