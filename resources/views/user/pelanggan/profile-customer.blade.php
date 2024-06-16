62d7f8ab108609adb3795f4e9b39431b45538f03
Diah Rahman <diahrahman10@gmail.com>
2024-06-16 15:31:40 +0700

    pcpc

--- resources/views/user/pelanggan/profile-customer.blade.php
+++ resources/views/user/pelanggan/profile-customer.blade.php
@@ -65,9 +65,14 @@
                                 @enderror
                             </div>
                         </div>
-
+                        <button type="submit" class="btn btn-primary">
+                            Simpan
+                        </button>
+                    </form>
+                    
+                    <form method="POST" action="">
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">Current Password</label>
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password">
                                 @error('password')
@@ -76,6 +81,18 @@
                                     </span>
                                 @enderror
                                 <small class="form-text text-muted">Leave blank if you don't want to change the password</small>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">New Password</label>
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                         </div>
 
