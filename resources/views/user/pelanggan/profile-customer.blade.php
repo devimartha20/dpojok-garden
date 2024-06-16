9e6c6356af6873364256dfd25fb2ea577b0b7146
Diah Rahman <diahrahman10@gmail.com>
2024-06-16 15:38:11 +0700

    kgkg

--- resources/views/user/pelanggan/profile-customer.blade.php
+++ resources/views/user/pelanggan/profile-customer.blade.php
@@ -13,7 +13,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Edit Account</div>
+                <div class="card-header">Edit Profile</div>
                 <div class="card-body">
                     <form method="POST" action="">
                         @csrf
@@ -65,9 +65,13 @@
                                 @enderror
                             </div>
                         </div>
-                        <button type="submit" class="btn btn-primary">
-                            Simpan
-                        </button>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Simpan
+                                </button>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
@@ -78,7 +82,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Edit Password</div>
+                <div class="card-header">Reset Password</div>
                     <form method="POST" action="">
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">Current Password</label>
